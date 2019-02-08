8e128996b1ca59374c0fc06cf1924e97693b48a2
Larry Hill <tomella1@gmail.com>
2019-02-08 21:57:20 +1030

    add the ability to vary rate of polling in URL

--- source/lt/app/app.js
+++ source/lt/app/app.js
@@ -7,12 +7,13 @@
 	.run(['$http', '$rootScope', '$timeout', function($http, $rootScope, $timeout) {
 		let parameters = UrlParameters.parameters();
       let number = parameters.id ? parameters.id : 1;
+      let period = parameters.period ? parameters.period : 10;  // seconds
 
       pollService();
 
 
 		function pollService() {
-			$timeout(pollService, 10000);
+			$timeout(pollService, period * 1000);
 			$http.get("/proxy/http://www.motogp.com/en/json/live_timing/" + number).then(function(response) {
 				$rootScope.$broadcast("lt.tick.data", response.data.lt);
 			});
